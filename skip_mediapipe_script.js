406755a7f0d5afda37905ecbda9087df538b22a6
Jiya Sama <chiakishinichi12@gmail.com>
2025-06-09 17:24:56 +0800

    Corrected skip_mediapipe_script.js

--- skip_mediapipe_script.js
+++ skip_mediapipe_script.js
@@ -55,7 +55,6 @@ async function validateImageQuality(inputImage, canvas) {
   	variance : -1
   };
 
-  const canvas = document.getElementById('canvas');
   const ctx = canvas.getContext('2d');
   
   canvas.width = inputImage.width / 2;
